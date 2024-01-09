c1c1adb779359b0c44fc6ace58df44d0d1f8f9e2
Lawice <129510417+Lawice@users.noreply.github.com>
2024-01-09 15:00:54 +0100

    Cross air + move

--- Assets/Script/ScMovement.cs
+++ Assets/Script/ScMovement.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScMovement : MonoBehaviour {
-    [Header("Movement")]
-   
-    [SerializeField] private float speed;
-
-    Vector3 moveDir;
     public Transform orientation;
-
     Rigidbody body;
 
+    [Header("~~~~Movement~~~~")]
+    [SerializeField] private float speed;
+    Vector3 moveDir;
+
+    [Header("~~~~GroundCheck~~~~")]
+    public float height;
+    [SerializeField] bool grounded;
+    public LayerMask Ground;
+    [SerializeField] float groundDrag;
 
     void Start() {
         body = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update() {
+        grounded = Physics.Raycast(transform.position, Vector3.down, height*0.5f + 0.2f, Ground);
+        if (grounded) { body.drag = groundDrag; }
+        else { body.drag = 0; }
     }
+    private void OnCollision3D(Collision collision) {
+        if (collision.gameObject.tag == "Ground") { grounded = true;}
+    }
+
 
     public void Move(Vector2 MoveInput) {
         moveDir = orientation.forward * MoveInput.y + orientation.right * MoveInput.x;
-        body.AddForce(moveDir.normalized * speed*5, ForceMode.Force);
+        body.AddForce(moveDir.normalized * speed * 10, ForceMode.Force);
+    }
+
+    private void SpeedControl() { 
+        Vector3 flatVelocity = new Vector3(body.velocity.x, 0f, body.velocity.z);
+        if(flatVelocity.magnitude > speed) {
+            Vector3 limitedVelocity = flatVelocity.normalized * speed;
+            body.velocity = new Vector3(limitedVelocity.x, body.velocity.y, limitedVelocity.z);
+        }
     }
 }
